3bd501eb290aee64442fb264b0112981d4565b4e
Chris Helgert <chris.helgert@online.de>
2017-11-21 12:56:39 +0100

    update dependencies und change syntax for hapi@17

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Hapi = require('hapi')
-const Inert = require('inert')
 const Boom = require('boom')
 const rp = require('request-promise-native')
 const tt = require('tinytime')
 const path = require('path')
 
-const server = new Hapi.Server()
+const server = new Hapi.Server({
+  port: process.env.PORT || '8080',
+  routes: { cors: true }
+})
+
 const template = tt('{YYYY}-{Mo}-{DD} {H}:{mm}:{ss}')
 
 const overrideHeaders = {
@@ -36,62 +39,57 @@ const getUrlFromQuery = (query) => {
   return `${url}&${searchParams}`
 }
 
-server.connection({
-  port: process.env.PORT || '8080',
-  routes: { cors: true }
-})
+(async function hapiRestProxy () {
+  await server.register({ plugin: require('inert') })
 
-server.register(Inert)
+  server.route({
+    method: '*',
+    path: '/',
+    handler: async (request, h) => {
+      const method = request.method.toUpperCase()
 
-server.route({
-  method: '*',
-  path: '/',
-  handler: async (request, reply) => {
-    const method = request.method.toUpperCase()
+      if (method === 'GET' && request.query.url === undefined) {
+        return h.file(path.join(__dirname, 'static', 'index.html'))
+      }
 
-    if (method === 'GET' && request.query.url === undefined) {
-      return reply.file(path.join(__dirname, 'static', 'index.html'))
-    }
+      if (!request.query.url) {
+        log(method, 400, undefined)
+        return Boom.badRequest('missing param url')
+      }
 
-    if (!request.query.url) {
-      log(method, 400, undefined)
-      return reply(Boom.badRequest('missing param url'))
-    }
+      const uri = getUrlFromQuery(request.query)
 
-    const uri = getUrlFromQuery(request.query)
+      try {
+        const result = await rp({
+          // hapi gives the request method in lowerCase
+          method,
+          uri,
+          body: request.payload || undefined,
+          headers: Object.assign({}, request.headers, overrideHeaders),
+          json: true
+        })
 
-    try {
-      const result = await rp({
-        // hapi gives the request method in lowerCase
-        method,
-        uri,
-        body: request.payload || undefined,
-        headers: Object.assign({}, request.headers, overrideHeaders),
-        json: true
-      })
+        log(method, 200, uri)
 
-      log(method, 200, uri)
+        return result
+      } catch (err) {
+        if (err.statusCode) {
+          log(method, err.statusCode, uri)
 
-      reply(result)
-    } catch (err) {
-      if (err.statusCode) {
-        log(method, err.statusCode, uri)
+          return Boom.create(err.statusCode, err.statusMessage, uri)
+        } else {
+          log(method, 500, uri)
 
-        return reply(Boom.create(err.statusCode, err.statusMessage, uri))
-      } else {
-        log(method, 500, uri)
-
-        reply(Boom.badImplementation())
+          return Boom.badImplementation()
+        }
       }
     }
-  }
-})
+  })
 
-server.start()
-  .then(() => {
+  try {
+    await server.start()
     console.info('Server running at:', server.info.uri)
-  })
-  .catch(() => {
-    console.error('There was an error starting the server')
-    return server.stop()
-  })
+  } catch (err) {
+    console.error('There was an error starting the server', err)
+  }
+}())
